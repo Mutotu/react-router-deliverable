3ea65caf589b507852a9f7fdcd50a127f31eeb12
joel-hughes-general-assembly <joel.hughes@generalassemb.ly>
2021-11-28 15:58:31 -0700

    Create favoriteMovies.js

---
+++ favoriteMovies.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    title: "Shazam",
+    year: 2019,
+    summary: "A boy is given the ability to become an adult superhero in times of need with a single magic word.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xnopI5Xtky18MPhK40cZAGAOVeV.jpg"
+  },
+  {
+    title: "Captian Marvel",
+    year: 2019,
+    summary: "The story follows Carol Danvers as she becomes one of the universe’s most powerful heroes when Earth is caught in the middle of a galactic war between two alien races. Set in the 1990s, Captain Marvel is an all-new adventure from a previously unseen period in the history of the Marvel Cinematic Universe.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aYDRwraSc8SXgot5jabtspJTWZB.jpg"
+  },
+  {
+    title: "Escape Room",
+    year: 2019,
+    summary: "Six strangers find themselves in circumstances beyond their control, and must use their wits to survive.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pHZPHngiO8hG78wAmAzMWcIrKdW.jpg",
+  },
+  {
+    title: "How to Train Your Dragon: The Hidden World",
+    year: 2019,
+    summary: "As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless’ discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup’s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg",
+  },
+  {
+    title: "Glass",
+    year: 2019,
+    summary: "In a series of escalating encounters, former security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/svIDTNUoajS8dLEo7EosxvyAsgJ.jpg",
+  },
+  {
+    title: "Doraemon the Movie: Nobita's Treasure Island",
+    year: 2018,
+    summary: "The story is based on Robert Louis Stevenson's Treasure Island novel.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xiLRClQmKSVAbiu6rgCRzNQjcSX.jpg",
+  },
+  {
+    title: "Dumbo",
+    year: 1941,
+    summary: "Dumbo is a baby elephant born with over-sized ears and a supreme lack of confidence. But thanks to his even more diminutive buddy Timothy the Mouse, the pint-sized pachyderm learns to surmount all obstacles.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4x9FmvdJ464Fg7A9XcbYSmxfVw3.jpg",
+  },
+  {
+    title: "Avengers: Infinity War",
+    year: 2018,
+    summary: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+    image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+  },
+]
