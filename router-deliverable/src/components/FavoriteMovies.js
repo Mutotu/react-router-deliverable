0f797420af0831531b9d425fdac9df24c7fccd67
mutotu <muttalip.aydemir@gmail.com>
2021-12-03 08:40:34 -0800

    showing movies

--- router-deliverable/src/components/FavoriteMovies.js
+++ router-deliverable/src/components/FavoriteMovies.js
@@ -1,53 +1,7 @@
-// import { useState } from "react";
-
-// const FavoriteMovie = (props) => {
-//   const [show, setShow] = useState(false);
-//   const cardMaker = (arg) => {
-//     if (show) {
-//       return (
-//         <div className='card'>
-//           <p>Year: {arg.year}</p>
-//           <p>Summary: {arg.summary}</p>
-//           <img src={arg.image} />
-//         </div>
-//       );
-//     } else {
-//       return null;
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <ul>
-//         <div className='movies'>
-//           {props.movies.map((movie, i) => {
-//             return (
-//               <li
-//                 className='list'
-//                 key={i}
-//                 onClick={() => {
-//                   setShow(true);
-
-//                 }}
-//               >
-//                 <span>{movie.title}</span>
-//                 {show ? cardMaker(movie) : null}
-//               </li>
-//             );
-//           })}
-//         </div>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default FavoriteMovie;
-
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const FavoriteMovie = (props) => {
-  const [show, setShow] = useState(!true);
-
+  const [show, setShow] = useState(false);
   const cardMaker = (arg) => {
     if (show) {
       return (
@@ -68,7 +22,13 @@ const FavoriteMovie = (props) => {
         <div className='movies'>
           {props.movies.map((movie, i) => {
             return (
-              <li className='list' key={i} onClick={() => {}}>
+              <li
+                className='list'
+                key={i}
+                onClick={() => {
+                  setShow(true);
+                }}
+              >
                 <span>{movie.title}</span>
                 {show ? cardMaker(movie) : null}
               </li>
@@ -81,3 +41,42 @@ const FavoriteMovie = (props) => {
 };
 
 export default FavoriteMovie;
+
+// import { useState, useEffect } from "react";
+
+// const FavoriteMovie = (props) => {
+//   const [show, setShow] = useState(!true);
+
+//   const cardMaker = (arg) => {
+//     if (show) {
+//       return (
+//         <div className='card'>
+//           <p>Year: {arg.year}</p>
+//           <p>Summary: {arg.summary}</p>
+//           <img src={arg.image} />
+//         </div>
+//       );
+//     } else {
+//       return null;
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <ul>
+//         <div className='movies'>
+//           {props.movies.map((movie, i) => {
+//             return (
+//               <li className='list' key={i} onClick={() => {}}>
+//                 <span>{movie.title}</span>
+//                 {show ? cardMaker(movie) : null}
+//               </li>
+//             );
+//           })}
+//         </div>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default FavoriteMovie;
